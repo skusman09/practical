925cd1bb9b99aea4c50ae3e36d362c14535c9502
skusman09 <imusman46@gmail.com>
2025-03-21 07:26:37 +0530

    ...

--- NLP/code/8C_shell_Named Entity Recognition with diagram using NLTK corpus - treebank.py
+++ NLP/code/8C_shell_Named Entity Recognition with diagram using NLTK corpus - treebank.py
@@ -7,8 +7,10 @@ nltk.download('treebank')
 from nltk.corpus import treebank_chunk
 treebank_chunk.tagged_sents() [0]
 
-treebank_chunk.chunked_sents() [0] #for Google colab
+treebank_chunk.chunked_sents() [0]
 
-#treebank_chunk.chunked_sents() [0].pretty_print() #for Idle
+#for Google colab
+treebank_chunk.chunked_sents() [0].pretty_print()
 
+#for Idle
 treebank_chunk.chunked_sents() [0].draw()
